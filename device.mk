7851b8a88f24b09a26d888148ca5da75da1fbee4
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    gauguin: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- device.mk
+++ device.mk
@@ -149,7 +149,7 @@ BOARD_HAVE_QCOM_FM := false
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
