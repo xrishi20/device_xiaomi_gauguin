e0bcf115924530aeccf0401099a580dde1887256
kawaaii <kawaaii@nocturn9x.space>
2022-07-31 04:06:16 +0000

    gauguin: Drop Google Camera GO
    This reverts commit 286d836043a64e7b926657a94f80aecf289904a4.
    
    Change-Id: I3f95b42500b6f96f1ff0d8235c5daa64aa267c2d

--- device.mk
+++ device.mk
@@ -167,10 +167,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
-# Camera
-PRODUCT_PACKAGES += \
-    GCamGO
-
 # Component overrides
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
