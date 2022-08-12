600f31d56146a3d87d5693db756972e4d8984b7a
Alexander Koskovich <akoskovich@pm.me>
2022-08-12 08:45:32 +0000

    gauguin: Drop component-overrides
     * Qualcomm no longer enables these for targets
       that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -172,10 +172,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Data Services
 $(call inherit-product, vendor/qcom/opensource/dataservices/dataservices_vendor_product.mk)
 
