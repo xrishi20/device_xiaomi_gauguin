9889f19be04b2a63934415832f8359be7f84ebc7
kawaaii <kawaaii@nocturn9x.space>
2022-08-27 08:52:09 +0545

    gauguin: Add displayconfig file
     - Template taken from [1] and dropped HBM.
     - Taken values from [2] Max Auto.
    
     - Fixes following logspam:
       E DisplayDeviceConfig: requesting nits when no mapping exists.
    
    [1]: https://android.googlesource.com/device/google/redfin/+/308328f60cbf7d445f5b089c5a5d00ba9ec880a1
    [2]: https://www.gsmarena.com/xiaomi_mi_10t_lite_5g-review-2203p3.php
    
    Change-Id: I1a14421de6b708051757c700407fd101cfbc2228

--- device.mk
+++ device.mk
@@ -191,6 +191,11 @@ $(call inherit-product, vendor/qcom/opensource/commonsys/display/config/display-
 PRODUCT_PACKAGES += \
     libtinyxml
 
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946545580055169.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946545580055169.xml \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_4630946480857061761.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946480857061761.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey
