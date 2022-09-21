5b4dd1a07ce349853b6d6135c185c398c46a8c28
iitzrohan <t.rohan1903@gmail.com>
2022-09-21 22:27:10 +0530

    gauguin: Add vendorsetup.sh
    Signed-off-by: iitzrohan <t.rohan1903@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+echo 'Starting to clone stuffs needed for your device'
+
+echo 'Cloning Vendor tree [1/3]'
+# Vendor Tree
+git clone --depth=1 https://github.com/iitzrohan/vendor_xiaomi_gauguin.git vendor/xiaomi/gauguin
+
+echo 'Cloning Kernel tree [2/3]'
+# Kernel Tree
+git clone --depth=1 https://github.com/iitzrohan/android_kernel_xiaomi_gauguin.git kernel/xiaomi/gauguin
+
+echo 'Cloning Proton clang [3/3]'
+# Proton Clang
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+
+echo 'Completed, Now proceeding to lunch'
